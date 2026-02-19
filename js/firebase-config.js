0f866fed6f66f44cba9753a264e8ebe95901d5bb
Macari Nicolae <macarinicolae2@gmail.com>
2026-02-19 20:27:50 +0200

    fix: update signup link to point to register page

---
+++ js/firebase-config.js
@@ -0,0 +1,24 @@
+// Firebase initialization
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-analytics.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBr1_qdvpZxZI1jvWvgDe4H7_u0nvjJyj0",
+  authDomain: "educheia.firebaseapp.com",
+  projectId: "educheia",
+  storageBucket: "educheia.firebasestorage.app",
+  messagingSenderId: "740416284743",
+  appId: "1:740416284743:web:b2022a18e9a26ee66b2962",
+  measurementId: "G-2J5GMZQ0G6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+export { app, analytics, auth };
